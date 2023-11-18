da7bf429165d1682b24bcb126dd3612a9d3972a1
woo <150315305+woo-cell@users.noreply.github.com>
2023-11-18 11:45:51 +0300

    added scoreCounter winMessage

--- script.js
+++ script.js
@@ -9,20 +9,34 @@ function getComputerChoice() {
     } else {
         computerChoice = "SCISSORS";
     }
+    console.log(computerChoice);
     return computerChoice;
 }
 
 // get user choice, if no choice or other choices, reprompt
 function getUserChoice() {
     let userChoice = "";
-    while (userChoice ==="" && userChoice !== "ROCK" && userChoice !== "PAPER" && userChoice !== "SCISSORS") {
+    while (userChoice !== "ROCK" && userChoice !== "PAPER" && userChoice !== "SCISSORS") {
         userChoice = String(prompt("Rock, paper or scissors?")).toUpperCase();
     }
     return userChoice;
 }
 
-function playRound() {
-    
+function playRound(computerSelection, playerSelection) {
+    let winner;
+    if (playerSelection === computerSelection) {
+        winner = "D";
+        return winner;
+    } else if (
+        playerSelection === "PAPER" && computerSelection === "ROCK"
+        || playerSelection === "SCISSORS" && computerSelection === "PAPER"
+        || playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        winner = "U";
+        return winner;
+    } else {
+        winner = "C"
+        return winner;
+    }
 }
 
 /* if
@@ -39,3 +53,34 @@ comp paper user rock : comp win
 comp sci user paper : comp win
 
 */
+
+function game(num) {
+    let score = 0;
+    let wMessage;
+    function scoreCounter(whoWins) {
+        if (whoWins==="D") {
+            score = score;
+        } else if (whoWins==="U") {
+            score ++;
+        } else if (whoWins==="C") {
+            score = score;
+        }
+        return score;
+    }
+    function winMessage(whoWins,point) {
+        if (whoWins==="D") {
+            wMessage = `It's a draw. You have ${point} points`;
+        } else if (whoWins==="U") {
+            wMessage = `You win, computer looses. You have ${point} points`;
+        } else if (whoWins==="C") {
+            wMessage = `You lose, computer wins. You have ${point} points`;
+        }
+        return wMessage;
+    }
+    for (let i = 0 ; i < num; i ++) {
+        let wi = playRound(getComputerChoice(),getUserChoice());
+        let sc = scoreCounter(wi);
+        let ms = winMessage(wi,sc);
+        console.log(ms);
+    }
+}
